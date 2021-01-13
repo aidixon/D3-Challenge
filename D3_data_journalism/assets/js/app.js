b6ab8d47816f28d5cc132f77bbb89803119ea627
Ashley Dixon <ashleydixon2012@gmail.com>
2021-01-13 16:28:08 -0700

    Created variables for margins

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,1 +1,14 @@
-// @TODO: YOUR CODE HERE!
+// Set the variables for height and width
+var svgWidth = 900;
+var svgHeight = 600;
+
+var margin = {
+    top: 25,
+    right: 45,
+    bottom: 60,
+    left: 150
+};
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
